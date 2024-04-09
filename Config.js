8a66947f08affd553c151c390bc2131ef9544229
SheddyRicch7 <166288386+SheddyRicch7@users.noreply.github.com>
2024-04-09 10:10:15 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownernumber = ['+2349117912013']
+global.ownername = "SheddyRicch"//owner name
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
